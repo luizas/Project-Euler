465c4bf72d32bb7949ad73b50e460cfdc42cc109
luizas <luiza.salek@gmail.com>
2016-08-07 23:55:42 +0100

    Create SmallestMultiple.java

---
+++ SmallestMultiple.java
@@ -0,0 +1,37 @@
+/*2520  is the smallest number that can be divided by each of the numbers from to without any remainder.
+What is the smallest positive number that is evenly divisible(divisible with no remainder) by all of the numbers from to ?*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int y=sc.nextInt();
+        
+        for(int z=0;z<y;z++)
+            { int x=sc.nextInt();
+        int m=1;
+     
+        for(int i=1; i<=x;i++)
+            {
+            m=m*i;
+                      }
+        int sz=m;
+        for(int i=m;i>0;i--)
+            {boolean t=true;
+            for(int j=1;j<=x;j++)
+                {
+                if(i%j==0){t=true;}
+                else{t=false;break;}
+            }
+             if(t==true){sz=i;}
+        }
+        System.out.println(sz);
+    }	
+    }
+}
